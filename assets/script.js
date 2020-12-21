4d076ec1bc036c2c94b7b2f655cadb8059ff74c5
Udunomancer <kyle717@gmail.com>
2020-12-21 00:45:15 -0500

    working API call with search

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,34 @@
 $(document).ready(function() {
     // ===DOM VARIABLES===
+    var currentWthrEl = $("#current-weather")
+    var currentWthrCityEl = $("#current-weather-city");
+    var currentWthrTempEl = $("#current-weather-temperature");
+    var currentWthrHumidEl = $("#current-weather-humidity");
+    var currentWthrWindEl = $("#current-weather-wind-speed");
+    var currentWthrUVEl = $("#current-weather-uv-index");
 
     // ===JS VARIABLES===
-    var currentWthrAPI = "api.openweathermap.org/data/2.5/weather?q="
+    var currentWthrAPI = "https://api.openweathermap.org/data/2.5/weather?q="
     var APIKey = "&appid=9bd6449387a57e80b45791c32b2fb94a";
     
 
     // ===FUNCTION DEFINITIONS===
     function citySearch(event) {
         event.preventDefault();
-        console.log($("#city").val().trim());
+        var city = $("#city").val().trim();
+        var queryURL = currentWthrAPI + city + APIKey;
+        
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+        })
+    }
+
+    function displayCurrentWthr() {
+        currentWthrEl.removeClass("visually-hidden")
+        currentWthrCityEl.text()
     }
 
 
