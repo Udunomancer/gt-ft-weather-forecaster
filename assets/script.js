8d77bb81ad62aa383d2fbea90ee30d9d81c5d67b
Udunomancer <kyle717@gmail.com>
2020-12-21 00:12:50 -0500

    confirmed working ajax call

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,21 @@
-console.log("Hello World");
-console.log($(this));
+$(document).ready(function() {
+    // ===DOM VARIABLES===
+
+    // ===JS VARIABLES===
+    var currentWthrAPI = "api.openweathermap.org/data/2.5/weather?q="
+    var APIKey = "&appid=9bd6449387a57e80b45791c32b2fb94a";
+    
+
+    // ===FUNCTION DEFINITIONS===
+
+
+    // ===FUNCTION CALLS===
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/weather?q=Atlanta&appid=9bd6449387a57e80b45791c32b2fb94a",
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+    })
+
+    // ===EVENT LISTENERS===
+});
