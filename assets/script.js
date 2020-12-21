6d37925f627ea0da5df6dfb48cf61564db38ed46
Udunomancer <kyle717@gmail.com>
2020-12-21 17:49:38 -0500

    moving functionality to weather object

--- assets/script.js
+++ assets/script.js
@@ -8,21 +8,91 @@ $(document).ready(function() {
     var currentWthrUVEl = $("#current-weather-uv-index");
 
     // ===JS VARIABLES===
-    var currentWthrAPI = "https://api.openweathermap.org/data/2.5/weather?q="
-    var APIKey = "&appid=9bd6449387a57e80b45791c32b2fb94a";
+    var weather = {
+        APIKey: "&appid=9bd6449387a57e80b45791c32b2fb94a",
+        city: "",
+        latitude: "",
+        longitute: "",
+        currentWthr: {
+            API: {
+                url: "https://api.openweathermap.org/data/2.5/weather",
+                query: "?q="
+            },
+            temperature: "",
+            humidity: "",
+            windSpeed: ""
+        },
+        uvIndex: {
+            API: {
+            url: "https://api.openweathermap.org/data/2.5/uvi",
+            latQ: "?lat=",
+            lonQ: "?lon=",
+            },
+            uvIndex: ""
+        },
+        makeCurrentWthrURL: function() {
+            return this.currentWthr.API.url + this.currentWthr.API.query + this.city + this.APIKey;
+        },
+        makeUVIndexURL: function() {
+
+        },
+        makeFiveDayURL: function() {
+
+        },
+        callAPI: function(calledURL) {
+            $.ajax({
+                url: calledURL,
+                method: "GET"
+            })
+        },
+        setCurrentWthr: function(response) {
+            this.city = response.name;
+            this.currentWthr.temperature = response.main.temp;
+            this.currentWthr.humidity = response.main.humidity;
+            this.currentWthr.windSpeed = response.wind.speed;
+        },
+        displayWthr: function() {
+            currentWthrEl.removeClass("visually-hidden")
+            currentWthrCityEl.text(this.city);
+            currentWthrTempEl.text("Temperature: " + this.currentWthr.temperature);
+            currentWthrHumidEl.text("Humidity: " + this.currentWthr.humidity);
+            currentWthrWindEl.text("Wind Speed: " + this.currentWthr.windSpeed);
+            console.log(response);
+        }
+    }
+    // var currentWthrAPI = {
+    //     url: "https://api.openweathermap.org/data/2.5/weather",
+    //     query: "?q=",
+    //     city: "",
+    //     APIKey: "&appid=9bd6449387a57e80b45791c32b2fb94a",
+    //     getWeather: function(newCity) {
+    //         this.city = newCity;
+    //         return this.url + this.query + this.city + this.APIKey;
+    //     }
+    // }
+    // var uvAPI = {
+    //     url: "https://api.openweathermap.org/data/2.5/uvi",
+    //     latQ: "?lat=",
+    //     latitude: "",
+    //     lonQ: "?lon=",
+    //     APIKey: "&appid=9bd6449387a57e80b45791c32b2fb94a"
+    // }
+   
     
 
     // ===FUNCTION DEFINITIONS===
     function citySearch(event) {
         event.preventDefault();
-        var city = $("#city").val().trim();
-        var queryURL = currentWthrAPI + city + APIKey;
+        weather.city = $("#city").val().trim();
         
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response) {
             displayCurrentWthr(response);
+            // $.ajax({
+
+            // })
         })
     }
 
@@ -32,6 +102,7 @@ $(document).ready(function() {
         currentWthrTempEl.text("Temperature: " + response.main.temp);
         currentWthrHumidEl.text("Humidity: " + response.main.humidity);
         currentWthrWindEl.text("Wind Speed: " + response.wind.speed);
+        console.log(response);
     }
 
     // ===FUNCTION CALLS===
